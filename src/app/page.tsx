4d7ef69fa778c00274af105eb2873bfc780c3752
david <austinedavid96@gmail.com>
2023-10-16 19:06:25 +0100

    made nice changes here

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ const Home = () => {
       <p>git one</p>
       <p>added redux here</p>
       <p>added more without redux branch</p>
+      <h1>lets check for works</h1>
+      <p>so, i don't actually need to switch before if works</p>
     </div>
   )
 }
